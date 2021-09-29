68dc0a3f36a7589925ca29e753ac0c50861118ca
ingyuHwang <dlsrb07@gmail.com>
2021-09-29 11:27:06 +0900

    chapter 9 done

--- microblog.py
+++ microblog.py
@@ -4,3 +4,9 @@ from app.models import User, Post
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'User': User, 'Post': Post}
+
+from livereload import Server
+
+if __name__ == '__main__':
+    server = Server(app.wsgi_app)
+    server.serve()
