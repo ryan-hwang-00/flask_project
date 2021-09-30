8d5791a580dde195103465f47f89f524d950cdcc
ingyuHwang <dlsrb07@gmail.com>
2021-09-30 10:37:35 +0900

    chapter 10 done

--- .flaskenv
+++ .flaskenv
@@ -1,5 +1,9 @@
 FLASK_APP = microblog.py
-FLASK_ENV=production
+FLASK_ENV = production
 
-MAIL_SERVER=localhost
-MAIL_PORT=8025
+MAIL_SERVER = 'smtp.mailtrap.io'
+MAIL_PORT = 2525
+MAIL_USERNAME =  '134fd26400821d'
+MAIL_PASSWORD = '81e299c934bf6b'
+MAIL_USE_TLS = True
+MAIL_USE_SSL = False
--- config.py
+++ config.py
@@ -7,10 +7,12 @@ class Config(object):
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     check_same_thread = False
+
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['your-email@example.com']
+    ADMINS = ['3305f5856a-9cfbf9@inbox.mailtrap.io']
+
     POSTS_PER_PAGE = 25
